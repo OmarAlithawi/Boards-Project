51d60e7f8c985124a44649505e249720ae92999d
Omar Alithawi <oadnan100@gmail.com>
2020-06-28 21:53:03 +0300

    Updates

---
+++ precache-manifest.0a3c889f21096c5869beaef0b2ca1e5f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca95aa0aae32d36b39d865061e1a8aba",
+    "url": "/OmarAlithawi/Boards-Project/index.html"
+  },
+  {
+    "revision": "630239f5c04e7fc2ba33",
+    "url": "/OmarAlithawi/Boards-Project/static/css/main.707cffbe.chunk.css"
+  },
+  {
+    "revision": "181d0bd8bb442f56014a",
+    "url": "/OmarAlithawi/Boards-Project/static/js/2.2ad505d1.chunk.js"
+  },
+  {
+    "revision": "fbfb5e42a41e7004782949e93fdf296a",
+    "url": "/OmarAlithawi/Boards-Project/static/js/2.2ad505d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "630239f5c04e7fc2ba33",
+    "url": "/OmarAlithawi/Boards-Project/static/js/main.591fd440.chunk.js"
+  },
+  {
+    "revision": "d9761e946232eaa3d719",
+    "url": "/OmarAlithawi/Boards-Project/static/js/runtime-main.dec41684.js"
+  },
+  {
+    "revision": "6c3912ffa5e93ddcb6aac6a70f8b1937",
+    "url": "/OmarAlithawi/Boards-Project/static/media/4380.6c3912ff.jpg"
+  },
+  {
+    "revision": "30f8e426130a371ea74696798a996335",
+    "url": "/OmarAlithawi/Boards-Project/static/media/accept.30f8e426.png"
+  },
+  {
+    "revision": "64a2760bcf7bb914ca0e0f84c20c0f11",
+    "url": "/OmarAlithawi/Boards-Project/static/media/contact-us.64a2760b.jpg"
+  },
+  {
+    "revision": "db132a5fedacd2621e4e6977b61af00d",
+    "url": "/OmarAlithawi/Boards-Project/static/media/contact.db132a5f.png"
+  },
+  {
+    "revision": "55f1a5f70574ba4d9e1537e9a10840fc",
+    "url": "/OmarAlithawi/Boards-Project/static/media/delete.55f1a5f7.png"
+  },
+  {
+    "revision": "08285c94dbf641f6489f42656e5d2dc8",
+    "url": "/OmarAlithawi/Boards-Project/static/media/edit.08285c94.png"
+  }
+]);
